a70648e5f5444bad24581bbc028a44b986e13916
jyang <jyang@7thonline.cn>
2018-02-14 13:59:59 +0800

    td style bank comprehensive

---
+++ TD/Comprehensive/EShowroom/ph_verify_ef.py
@@ -0,0 +1,122 @@
+PH_AT_VE_004=[{'color_code': '001','color_name': 'BLACK','season': 'SPRING','year': '2018'}]
+
+PH_AT_MA_004=[{'color_code': '001','color_name': 'BLACK','season': 'SPRING','year': '2018'}] #Delected
+
+PH_AT_VE_005=[{'color_code': '002','color_name': 'WHITE','season': 'SPRING','year': '2018'}]
+
+PH_AT_MA_005=[{'color_code': '001','color_name': 'BLACK','season': 'SPRING','year': '2018'}]
+
+PH_AT_VE_006=[{'color_code': '001','color_name': 'BLACK','season': 'SPRING','year': '2018'}] #ensure in ph style page
+
+PH_AT_MA_006=[{'color_code': '001','color_name': 'BLACK','season': 'SPRING','year': '2018'}]
+
+PH_AT_VE_007=[{'color_code': '001','color_name': 'BLACK','season': 'SPRING','year': '2018'}] #ensure in ph style page
+
+PH_AT_MA_007=[{'color_code': '001','color_name': 'BLACK','season': 'SPRING','year': '2018'},{'color_code': '002','color_name': 'WHITE','season': 'SPRING','year': '2018'}]
+
+PH_AT_VE_008=[{'color_code': '001','color_name': 'BLACK','season': 'SPRING','year': '2017'}] #ensure in ph style page
+
+PH_AT_MA_008=[{'color_code': '001','color_name': 'BLACK','season': 'SPRING','year': '2018'},{'color_code': '002','color_name': 'WHITE','season': 'FALL','year': '2018'},{'color_code': '001','color_name': 'BLACK','season': 'SPRING','year': '2017'}]
+
+PH_AT_VE_009=[{'color_code': '001','color_name': 'BLACK','season': 'SPRING','year': '2018'}]
+
+PH_AT_MA_009=[{'color_code': '001','color_name': 'BLACK','season': 'FALL','year': '2018'}]
+
+PH_AT_VE_010=[{'color_code': '001','color_name': 'BLACK','season': 'SPRING','year': '2017'}] #ensure in ph style page
+
+# order by color name
+PH_AT_MA_010=[{'color_code': '001','color_name': 'BLACK','season': 'SPRING','year': '2018'},{'color_code': '004','color_name': 'BLUE','season': 'FALL','year': '2018'},{'color_code': '003','color_name': 'RED','season': 'SPRING','year': '2018'},{'color_code': '002','color_name': 'WHITE','season': 'FALL','year': '2018'}]
+
+PH_AT_VE_011=[{'color_code': '001','color_name': 'BLACK','season': 'SPRING','year': '2017'}] #ensure in ph style page
+
+PH_AT_MA_011=[{'color_code': '003','color_name': 'RED','season': 'FALL','year': '2018'},{'color_code': '002','color_name': 'WHITE','season': 'SPRING','year': '2018'}]
+
+PH_AT_VE_012=[{'color_code': '001','color_name': 'BLACK','season': 'SPRING','year': '2018'}]
+
+PH_AT_MA_012=[{'color_code': '001','color_name': 'BLACK','season': 'SPRING','year': '2018'}] #Archived
+
+PH_AT_VE_013=[{'color_code': '001','color_name': 'BLACK','season': 'SPRING','year': '2018'}]
+PH_AT_MA_013=[{'color_code': '001','color_name': 'BLACK','season': 'SPRING','year': '2018'}]
+
+PH_AT_VE_014=[{'color_code': '001','color_name': 'BLACK','season': 'SPRING','year': '2018'}]
+PH_AT_MA_014=[{'color_code': '001','color_name': 'BLACK','season': 'SPRING','year': '2018'}]
+
+PH_AT_VE_015=[{'color_code': '001','color_name': 'BLACK','season': 'SPRING','year': '2018'},{'color_code': '002','color_name': 'WHITE','season': 'SPRING','year': '2018'}]
+PH_AT_MA_015=[{'color_code': '002','color_name': 'WHITE','season': 'SPRING','year': '2018'}]
+
+PH_AT_VE_016=[{'color_code': '001','color_name': 'BLACK','season': 'SPRING','year': '2018'},{'color_code': '002','color_name': 'WHITE','season': 'SPRING','year': '2018'}]
+PH_AT_MA_016=[{'color_code': '001','color_name': 'BLACK','season': 'SPRING','year': '2018'},{'color_code': '002','color_name': 'WHITE','season': 'SPRING','year': '2018'}]
+
+PH_AT_VE_017=[{'color_code': '001','color_name': 'BLACK','season': 'SPRING','year': '2018'},{'color_code': '003','color_name': 'RED','season': 'SPRING','year': '2018'},{'color_code': '002','color_name': 'WHITE','season': 'SPRING','year': '2018'}]
+PH_AT_MA_017=[{'color_code': '001','color_name': 'BLACK','season': 'SPRING','year': '2018'},{'color_code': '003','color_name': 'RED','season': 'SPRING','year': '2018'},{'color_code': '002','color_name': 'WHITE','season': 'SPRING','year': '2018'},
+              {'color_code': '002', 'color_name': 'WHITE', 'season': 'SPRING', 'year': '2019'},{'color_code': '002','color_name': 'WHITE','season': 'SPRING','year': '2017'}]
+
+PH_AT_VE_018=[{'color_code': '001','color_name': 'BLACK','season': 'SPRING','year': '2018'},{'color_code': '002','color_name': 'WHITE','season': 'SPRING','year': '2018'}]
+PH_AT_MA_018=[{'color_code': '004','color_name': 'BLUE','season': 'SPRING','year': '2018'},{'color_code': '003','color_name': 'RED','season': 'SPRING','year': '2018'}]
+
+PH_AT_VE_019=[{'color_code': '001','color_name': 'BLACK','season': 'SPRING','year': '2018'},{'color_code': '002','color_name': 'WHITE','season': 'SPRING','year': '2018'}]
+PH_AT_MA_019=[{'color_code': '001','color_name': 'BLACK','season': 'SPRING','year': '2019'},{'color_code': '002','color_name': 'WHITE','season': 'SPRING','year': '2019'}]
+
+PH_AT_VE_020=[{'color_code': '001','color_name': 'BLACK','season': 'SPRING','year': '2018'},{'color_code': '002','color_name': 'WHITE','season': 'SPRING','year': '2018'}]
+PH_AT_MA_020=[{'color_code': '004','color_name': 'BLUE','season': 'FALL','year': '2019'},{'color_code': '003','color_name': 'RED','season': 'SPRING','year': '2019'}]
+
+PH_AT_VE_021=[{'color_code': '001','color_name': 'BLACK','season': 'SPRING','year': '2018'},{'color_code': '003','color_name': 'RED','season': 'SPRING','year': '2018'},{'color_code': '002','color_name': 'WHITE','season': 'SPRING','year': '2018'}]
+PH_AT_MA_021=[{'color_code': '004','color_name': 'BLUE','season': 'SPRING','year': '2018'},{'color_code': '005','color_name': 'GREEN','season': 'SPRING','year': '2018'},{'color_code': '006','color_name': 'YELLOW','season': 'SPRING','year': '2018'},
+              {'color_code': '002', 'color_name': 'WHITE', 'season': 'SPRING', 'year': '2019'},{'color_code': '002','color_name': 'WHITE','season': 'SPRING','year': '2017'}]
+
+PH_AT_VE_022=[{'color_code': '001','color_name': 'BLACK','season': 'SPRING','year': '2018'},{'color_code': '003','color_name': 'RED','season': 'SPRING','year': '2018'},{'color_code': '002','color_name': 'WHITE','season': 'SPRING','year': '2018'}]
+PH_AT_MA_022=[{'color_code': '001','color_name': 'BLACK','season': 'SPRING','year': '2019'},{'color_code': '003','color_name': 'RED','season': 'SPRING','year': '2019'},{'color_code': '002','color_name': 'WHITE','season': 'SPRING','year': '2019'},
+              {'color_code': '001', 'color_name': 'BLACK', 'season': 'SPRING', 'year': '2017'},
+              {'color_code': '003', 'color_name': 'RED', 'season': 'SPRING', 'year': '2017'},
+              {'color_code': '002', 'color_name': 'WHITE', 'season': 'SPRING', 'year': '2017'}]
+
+PH_AT_VE_023=[{'color_code': '001','color_name': 'BLACK','season': 'SPRING','year': '2018'},{'color_code': '002','color_name': 'WHITE','season': 'SPRING','year': '2018'}]
+PH_AT_MA_023=[{'color_code': '001','color_name': 'BLACK','season': 'SPRING','year': '2018'}]
+
+PH_AT_VE_024=[{'color_code': '001','color_name': 'BLACK','season': 'SPRING','year': '2018'},{'color_code': '002','color_name': 'WHITE','season': 'SPRING','year': '2018'}]
+PH_AT_MA_024=[{'color_code': '001','color_name': 'BLACK','season': 'SPRING','year': '2018'}]
+
+PH_AT_VE_025=[{'color_code': '001','color_name': 'BLACK','season': 'SPRING','year': '2018'},{'color_code': '002','color_name': 'WHITE','season': 'SPRING','year': '2018'}]
+PH_AT_MA_025=[{'color_code': '001','color_name': 'BLACK','season': 'SPRING','year': '2018'}]
+
+PH_AT_VE_026=[{'color_code': '001','color_name': 'BLACK','season': 'SPRING','year': '2018'},{'color_code': '002','color_name': 'WHITE','season': 'SPRING','year': '2018'}]
+PH_AT_MA_026=[{'color_code': '001','color_name': 'BLACK','season': 'SPRING','year': '2018'}]
+
+PH_AT_VE_027=[{'color_code': '001','color_name': 'BLACK','season': 'SPRING','year': '2018'},{'color_code': '003','color_name': 'RED','season': 'SPRING','year': '2018'},{'color_code': '002','color_name': 'WHITE','season': 'SPRING','year': '2018'}]
+PH_AT_MA_027=[{'color_code': '001','color_name': 'BLACK','season': 'SPRING','year': '2018'}]
+
+PH_AT_VE_028=[{'color_code': '003','color_name': 'RED','season': 'SPRING','year': '2018'},{'color_code': '002','color_name': 'WHITE','season': 'SPRING','year': '2018'}]
+PH_AT_MA_028=[{'color_code': '001','color_name': 'BLACK','season': 'SPRING','year': '2018'}]
+
+PH_AT_MA_029=[{'color_code': '001','color_name': 'BLACK','season': 'SPRING','year': '2018'}]
+PH_AT_VE_029=[{'color_code': '001','color_name': 'BLACK','season': 'SPRING','year': '2017'}]
+
+
+PH_AT_MA_030=[{'color_code': '002','color_name': 'WHITE','season': 'SPRING','year': '2018'}]
+PH_AT_VE_030=[{'color_code': '001','color_name': 'BLACK','season': 'SPRING','year': '2018'}]
+
+PH_AT_MA_031=[{'color_code': '002','color_name': 'WHITE','season': 'SPRING','year': '2018'}]
+PH_AT_VE_031=[{'color_code': '001','color_name': 'BLACK','season': 'SPRING','year': '2017'}]
+
+PH_AT_MA_032=[{'color_code': '001','color_name': 'BLACK','season': 'SPRING','year': '2018'},
+              {'color_code': '003','color_name': 'RED', 'season': 'SPRING', 'year': '2018'},
+              {'color_code': '002','color_name': 'WHITE','season': 'SPRING','year': '2018'}]
+PH_AT_VE_032=[{'color_code': '001','color_name': 'BLACK','season': 'SPRING','year': '2017'},
+              {'color_code': '003','color_name': 'RED', 'season': 'SPRING', 'year': '2017'},
+              {'color_code': '002','color_name': 'WHITE','season': 'SPRING','year': '2017'}]
+
+PH_AT_MA_033 = [{'color_code': '003','color_name': 'RED', 'season': 'SPRING', 'year': '2018'},
+                {'color_code': '002','color_name': 'WHITE','season': 'SPRING','year': '2018'}]
+PH_AT_VE_033 = [{'color_code': '001','color_name': 'BLACK', 'season': 'SPRING', 'year': '2018'},
+                {'color_code': '004','color_name': 'BLUE','season': 'SPRING','year': '2018'}]
+
+PH_AT_MA_034 = [{'color_code': '003','color_name': 'RED', 'season': 'SPRING', 'year': '2018'},
+                {'color_code': '002','color_name': 'WHITE','season': 'SPRING','year': '2018'}]
+PH_AT_VE_034 = [{'color_code': '001','color_name': 'BLACK', 'season': 'SPRING', 'year': '2017'},
+                {'color_code': '004','color_name': 'BLUE','season': 'SPRING','year': '2017'}]
+
+PH_AT_MA_035 = [{'color_code': '001','color_name': 'BLACK', 'season': 'SPRING', 'year': '2018'}]
+PH_AT_VE_035 = [{'color_code': '004','color_name': 'BLUE','season': 'SPRING','year': '2018'},
+                {'color_code': '003', 'color_name': 'RED', 'season': 'SPRING', 'year': '2018'},
+                {'color_code': '002', 'color_name': 'WHITE', 'season': 'SPRING', 'year': '2018'},]
+
---
+++ TD/Comprehensive/EShowroom/style_bank_test_variables_ef.py
@@ -0,0 +1,140 @@
+SB001={
+    'season_year': 'FALL-2018',
+    'searched_style': 'ES001A'
+}
+
+SB002={
+    'season_year': 'RESORT-2017',
+    'searched_style': 'ES001A'
+}
+
+SB003={
+    'searched_style': 'NOAUTOEXIST'
+}
+
+SB004={
+    '1Year': ['2018'],
+    '2Fashion Season': ['FALL'],
+    '3Story': ['AUTOTESTSTORY']
+}
+
+SB005={
+    'searched_style': 'ES001A'
+}
+
+SB006={
+    'searched_style': 'ES001A',
+    '1Year': ['2018'],
+    '2Fashion Season': ['FALL'],
+    '3Story': ['AUTOTESTSTORY']
+}
+
+SB007={
+    'searched_style': 'ES001A',
+    '1Year': ['2017'],
+    '2Fashion Season': ['RESORT']
+}
+
+SB008={
+    'searched_style': 'NOAUTOEXIST'
+}
+
+SB009={
+    'searched_style': 'AUTO-PH-009A',
+    'color_codes': ['001'],
+    'color_descs': ['Black'],
+    'season': 'FALL',
+    'year': '2018',
+    'classname': 'J - JACKETS',
+    'department': 'MISSY',
+    'division': 'WOMENSWEAR',
+    'groupcategory': 'PLACEHOLDER'
+}
+
+SB012={
+    'searched_style': 'AUTO-PH-009A',
+    '1Year': ['2018'],
+    '2Fashion Season': ['FALL'],
+    '3Story': ['PLACEHOLDER']
+}
+
+SB013={
+    'searched_style': 'AUTO-PH-009A',
+    '1Year': ['2018'],
+    '2Fashion Season': ['FALL'],
+    '3Story': ['AUTOTESTSTORY']
+}
+
+SB014={
+    '1Year': ['2018'],
+    '2Fashion Season': ['FALL'],
+    '3Story': ['PLACEHOLDER']
+}
+
+SB015={
+    'searched_style': 'NOAUTOEXIST',
+    '1Year': ['2018'],
+    '2Fashion Season': ['FALL'],
+    '3Story': ['AUTOTESTSTORY']
+}
+
+SB040={
+    'searched_style': 'AUTO-PH-009A',
+    '1Year': ['2018'],
+    '2Fashion Season': ['FALL'],
+}
+
+PH001={
+    'searched_style': 'PH-A-M-AVG',
+    'season_year': 'FALL-2018'
+}
+
+PH004={
+    '1Year': ['2018'],
+    '2Fashion Season': ['FALL'],
+    '3Story': ['PLACEHOLDER']
+}
+
+PH005={
+    'searched_style': 'PH-A-M-AVG'
+}
+
+PH006={
+    'searched_style': 'PH-A-M-MAX',
+    '1Year': ['2018'],
+    '2Fashion Season': ['FALL'],
+    '3Story': ['PLACEHOLDER']
+}
+
+PH007={
+    'searched_style': 'PH-A-M-MAX',
+    '1Year': ['2018'],
+    '2Fashion Season': ['SPRING'],
+    '3Story': ['PLACEHOLDER']
+}
+
+PHEXISTINGSTYLE='PH-A-M-AVG'
+PHNOTEXISTINGSTYLE='NONEXISTINGSTYLE'
+
+PHFILTERMATCHED={
+    'searched_style':PHEXISTINGSTYLE,
+    '1Year': ['2018'],
+    '2Fashion Season': ['FALL']
+}
+
+PHFILTERMISMATCHED={
+    'searched_style': PHEXISTINGSTYLE,
+    '1Year': ['2018'],
+    '2Fashion Season': ['SPRING']
+}
+
+PHONLYFILTER={
+    '1Year': ['2018'],
+    '2Fashion Season': ['SPRING']
+}
+
+PHFILTERUNEXISTINGSTYLE={
+    'searched_style': PHNOTEXISTINGSTYLE,
+    '1Year': ['2018'],
+    '2Fashion Season': ['SPRING']
+}
